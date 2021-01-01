c1b68dbf8fa38e740b668fd67c568dccba5c7c06
rami.atrouni <ramiatrouni@gmail.com>
2021-01-01 14:23:13 +0200

    change routing from link to actual react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap'
 import Rating from './Rating';
 
@@ -6,16 +7,16 @@ import Rating from './Rating';
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant="top"></Card.Img>
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as="div">
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
 
                 <Card.Text as="div">
                     <Rating value={product.rating}
