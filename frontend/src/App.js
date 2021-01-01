63b529f6833f0e3955d5d1cfe50dc98dd4ae4474
rami.atrouni <ramiatrouni@gmail.com>
2021-01-01 13:15:24 +0200

    Install bootstrap fontawesome and init Header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome here</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome here</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
